f4dc786ce755875c6b140654c7c7ac8087c972bd
Schwyi <lucasmirandoliveira@gmail.com>
2021-06-14 20:32:55 -0300

    document is not available during server side rendering error fixed

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,7 +4,7 @@ import { css } from "@emotion/react"
 import { rhythm } from "../utils/typography"
 import DarkModeToggler from "./DarkModeToggler"
 import ClientOnly from "./ClientOnly"
-import Particles from 'react-particles-js';
+ 
 const isActive =
   propsIfActive =>
   ({ isPartiallyCurrent, href, location }) => {
@@ -156,119 +156,10 @@ export default function Layout({ children }) {
       position:relative;
            z-index: 10
           `}>{children}</div>
-          { typeof window !== 'undefined'? (
- <Particles params={{
-  "particles": {
-    "number": {
-      "value": 80,
-      "density": {
-        "enable": true,
-        "value_area": 800
-      }
-    },
-    "color": {
-      "value": "#000000"
-    },
-    "shape": {
-      "type": "circle",
-      "stroke": {
-        "width": 0,
-        "color": "#000000"
-      },
-      "polygon": {
-        "nb_sides": 5
-      },
-      "image": {
-        "src": "img/github.svg",
-        "width": 100,
-        "height": 100
-      }
-    },
-    "opacity": {
-      "value": 0.5,
-      "random": false,
-      "anim": {
-        "enable": false,
-        "speed": 1,
-        "opacity_min": 0.1,
-        "sync": false
-      }
-    },
-    "size": {
-      "value": 3,
-      "random": true,
-      "anim": {
-        "enable": false,
-        "speed": 40,
-        "size_min": 0.1,
-        "sync": false
-      }
-    },
-    "line_linked": {
-      "enable": true,
-      "distance": 150,
-      "color": "#000000",
-      "opacity": 0.4,
-      "width": 1
-    },
-    "move": {
-      "enable": true,
-      "speed": 6,
-      "direction": "bottom",
-      "random": false,
-      "straight": false,
-      "out_mode": "out",
-      "bounce": false,
-      "attract": {
-        "enable": false,
-        "rotateX": 600,
-        "rotateY": 1200
-      }
-    }
-  },
-  "interactivity": {
-    "detect_on": "canvas",
-    "events": {
-      "onhover": {
-        "enable": true,
-        "mode": "grab"
-      },
-      "onclick": {
-        "enable": false,
-        "mode": "push"
-      },
-      "resize": true
-    },
-    "modes": {
-      "grab": {
-        "distance": 224.28415972354892,
-        "line_linked": {
-          "opacity": 1
-        }
-      },
-      "bubble": {
-        "distance": 400,
-        "size": 40,
-        "duration": 2,
-        "opacity": 8,
-        "speed": 3
-      },
-      "repulse": {
-        "distance": 200,
-        "duration": 0.4
-      },
-      "push": {
-        "particles_nb": 4
-      },
-      "remove": {
-        "particles_nb": 2
-      }
-    }
-  },
-  "retina_detect": true
-}}/>
-          ): null
-          }
+         
+
+      
+        
      
     </div>
   )
